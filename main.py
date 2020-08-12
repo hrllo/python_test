d04ac6df02da7d1deaae9a0ba43ab0b6a45f174d
hrllo <1738995905@qq.com>
2020-08-12 13:32:20 +0800

    DELETE

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import psycopg2
-
-conn = psycopg2.connect(database="postgres", user="postgres", password="123456", host="127.0.0.1", port="5432")
-
-print("Opened database successfully")
